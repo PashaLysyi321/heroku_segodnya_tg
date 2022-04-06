2586096d978c88a121dfdcb5fa72dd4f78d8f999
PashaLysyi321 <lysyi.pavlos@gmail.com>
2022-04-06 18:38:32 +0300

    first commit 2

--- process.py
+++ process.py
@@ -1,11 +1,39 @@
 import flask
+from telethon import TelegramClient, functions, types
+from asyncio import run
+import asyncio
+from telethon.tl.functions.contacts import ResolveUsernameRequest
+from datetime import date
+import pandas as pd
+import re
+import requests
+import json
 
 app = flask.Flask(__name__)
-
-@app.route('/')
-@app.route('/home')
+@app.route('/download')
 def home():
-    return 'Hello World'
+    channel = 'Segodnya_life'
+    url = """https://api.telegram.org/bot986604365:AAHiOKpoNY3YNF71Jav26J6ONSFnDft9rJ0/getChatMemberCount?chat_id=@"""+channel
+    r = requests.get(url)
+    numberofusers = json.loads(r.text).get('result')
+    name = 'anon'
+    chat = 'Segodnya_life'
+    api_id = 16865639
+    api_hash = '81aacb85d2a6e578ae6584d00f18866c'
+    r = re.compile("[^а-яА-Я?!,.:%;№$""0-9''\s\- ]+")
+    data = {'Channel':[], 'Number of subscribers':[], 'Text': [], 'Date': [], 'Views':[], 'Forwards':[],'Pinned':[]}   
+    df = pd.DataFrame(data)
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    with TelegramClient(name, api_id, api_hash) as client:
+        for message in client.iter_messages(chat):
+            if message.date.date() != date.today():
+                break
+            if message.text !='':
+                df.loc[len(df)] = [channel, numberofusers, r.sub('',message.text.replace('\n',' ')), str(message.date), message.views, message.forwards, message.pinned]
+    print(df)
+    df.to_excel("parsing_tg.xlsx")
+    return flask.send_file("parsing_tg.xlsx", as_attachment=True)
 
 if __name__ == "__main__":
     app.run()
